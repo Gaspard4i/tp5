e0e31826fa9b63a267dc60d448061c0a09d2f62a
Thomas Fritsch <thomas@uidlt.fr>
2020-07-06 22:39:54 +0200

    format

--- src/VideoDetail.js
+++ src/VideoDetail.js
@@ -19,7 +19,9 @@ export default class VideoDetail extends Component {
 		const { title, description, file, likes, dislikes } = this.state.video;
 		return (
 			<div className="videoDetail">
-				<button className="backButton" onClick={() => this.props.push('list')}>&lt; Retour</button>
+				<button className="backButton" onClick={() => this.props.push('list')}>
+					&lt; Retour
+				</button>
 				<video
 					style={{ width: '100%', backgroundColor: 'black' }}
 					height="400"
