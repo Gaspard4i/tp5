f5ce8749ca13a2f32bd89dbfae09ad37a9e566e9
Thomas Fritsch <thomas@uidlt.fr>
2020-07-06 18:11:12 +0200

    correction lien VideoItem
    thumbnail -> file

--- src/VideoItem.js
+++ src/VideoItem.js
@@ -2,10 +2,9 @@ import Component from './Component.js';
 import Img from './Img.js';
 
 export default class VideoItem extends Component {
-	constructor({ title, description, thumbnail }) {
-		const url = `https://source.unsplash.com/${thumbnail}/600x340`;
-		super('a', { name: 'href', value: url }, [
-			new Img(url),
+	constructor({ title, description, thumbnail, file }) {
+		super('a', { name: 'href', value: `./uploads/${file}` }, [
+			new Img(`https://source.unsplash.com/${thumbnail}/600x340`),
 			new Component('section', { name: 'class', value: 'infos' }, [
 				new Component('h4', null, title),
 				new Component('p', null, description),
