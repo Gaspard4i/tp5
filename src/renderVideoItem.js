5d0de6847f2209763fd8a0d6aad8c5958c0bb9ca
Thomas Fritsch <thomas@uidlt.fr>
2024-05-28 21:33:20 +0200

    remplacement urls source.unsplash (down)

--- src/renderVideoItem.js
+++ src/renderVideoItem.js
@@ -5,7 +5,7 @@ import renderImg from './renderImg.js';
 export default ( { title, description, file, thumbnail } ) =>
 	// plein d'appels imbriqués 😬
 	renderElement( 'a', { name: 'href', value: `./uploads/${file}` }, [
-		renderImg( `https://source.unsplash.com/${thumbnail}/600x340` ),
+		renderImg( `https://unsplash.uidlt.fr/${thumbnail}/600x340` ),
 		renderElement( 'section', { name: 'class', value: 'infos' }, [
 			renderElement( 'h4', null, title ),
 			renderElement( 'p', null, description ),
