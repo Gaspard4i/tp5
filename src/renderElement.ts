c45e53a97f6888b859c0eaff291e5f5b85965b45
Thomas Fritsch <thomas@uidlt.fr>
2024-12-03 17:17:49 +0100

    ajout commentaires renderElement

--- src/renderElement.ts
+++ src/renderElement.ts
@@ -1,12 +1,16 @@
-type Attribute = null | {
+type Attribute = {
 	name: string,
 	value: string,
 };
 type Children = string | string[];
 
-export default function renderElement( tagName:string = 'div', attribute?: Attribute, children?:Children ) {
-	let attributeHtml = '';
-	if ( attribute ) {
+export default function renderElement(
+	tagName:string = 'div', // paramètre facultatif avec une valeur par défaut
+	attribute?: Attribute | null, // paramètre facultatif grâce au "?" devant les ":" (null autorisé)
+	children?:Children // paramètre facultatif (null interdit)
+) {
+	let attributeHtml = ''; // inférence de type : attributeHtml est détecté comme string
+	if ( attribute ) { // type coercion : attribute est "converti" en booléen : vaut false si null ou undefined
 		attributeHtml = `${attribute.name}="${attribute.value}"`;
 	}
 
